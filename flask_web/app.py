faf3ad171e7ec1b6a07d45e5b46c8814797255c3
Fredrik Fornwall <fredrik@fornwall.net>
2021-03-12 00:53:40 +0100

    Test code

--- flask_web/app.py
+++ flask_web/app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, request
 from PIL import Image
+from tempfile import NamedTemporaryFile
+from fastai.learner import load_learner
+
+# https://stackoverflow.com/questions/12984426/python-pil-ioerror-image-file-truncated-with-big-images
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
 app = Flask(__name__)
+learner = load_learner("model.pkl")
 
 
 @app.route("/")
@@ -13,9 +20,14 @@ def hello_world():
 @app.route("/", methods=["POST"])
 def upload_file():
     uploaded_file = request.files["file"]
+    temp = NamedTemporaryFile()
+    uploaded_file.save(temp)
     try:
-        image = Image.open(uploaded_file.stream)
-        return f"Width: {image.width}, Height: {image.height}"
+        #image = Image.open(uploaded_file.stream)
+        #image = Image.open(temp.name)
+        #return f"Width: {image.width}, Height: {image.height}"
+        prediction = learner.predict(temp.name)
+        return f"Prediction: {prediction}"
     except Exception as e:
         return f"File: {upload_file}, Exception: {e}"
 
